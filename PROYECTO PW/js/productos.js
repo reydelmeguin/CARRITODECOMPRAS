9ac80cc568e09c7cbfcd55fd29520cfc2a6afbaf
reydelmeguin <160188069+reydelmeguin@users.noreply.github.com>
2024-11-27 11:37:47 -0600

    Add files via upload

---
+++ PROYECTO PW/js/productos.js
@@ -0,0 +1,41 @@
+const objetos = [
+    {
+        id:1,
+        nombre: "objeto 1",
+        img: "1.jpg",
+        precio: 100,
+
+    },
+
+    {
+        id:2,
+        nombre: "objeto 2",
+        img: "",
+        precio: 200,
+
+    },
+
+    {
+        id:3,
+        nombre: "objeto 3",
+        img: "",
+        precio: 300,
+
+    },
+
+    {
+        id:4,
+        nombre: "objeto 4",
+        img: "",
+        precio: 400,
+
+    },
+
+    {
+        id:5,
+        nombre: "objeto 5",
+        img: "",
+        precio: 500,
+
+    },
+]
